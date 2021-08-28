b5f6a5c0bda98525e51fecbbba4c14a68a55ab2c
Mako0530 <zhongtiancheng79@gmail.com>
2021-08-28 23:39:34 +0900

    ログイン機能、トップページの作成、レビュー投稿

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Review;
+
+class ReviewController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function create()
+    {
+        return view('review');
+    }
+
+    public function store(Request $request)
+    {
+        $post = $request->all();
+
+        if ($request->hasFile('image')) {
+            $request->file('image')->store('/public/images');
+            $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body'], 'image' => $request->file('image')->hashName()];
+        } else {
+            $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body']];
+        }
+
+        Review::insert($data);
+
+        return redirect('/');
+    }
+}
